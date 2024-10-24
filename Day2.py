ebd1594f7fa82dcc5357101bdafd7d1df2891e8a
Robyn Martin <robynmartin954@gmail.com>
2024-10-24 19:41:22 -0400

    Work on list and loop

---
+++ Day2.py
@@ -0,0 +1,10 @@
+# List
+
+DataStructure = ['Linkedlist', 'Queue', 'Stack', 'BinaryTree', 'HashTable']
+week = 7
+print(f"WEEK {week} " " "  + ": this week we reviewed" + " " + DataStructure[-1])
+
+DataStructure.insert(0,"ArrayList") # add to the start
+
+for lessons in DataStructure:
+    print(lessons)
